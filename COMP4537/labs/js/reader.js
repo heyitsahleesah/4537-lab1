1e972f3062d3354adc60b40597f5c2f85ecbff00
Alissa <alissa.graham88@gmail.com>
2023-09-16 00:48:44 -0700

    fixed reader lag

--- COMP4537/labs/js/reader.js
+++ COMP4537/labs/js/reader.js
@@ -6,35 +6,22 @@ class readText {
         this.container.appendChild(this.row);
         this.row.innerHTML = text;
     }
-    
-   
-        // const currentNotes = [];
-
-        // // pull the text and put it in the new div row
-        // for (let i = 0; i < localStorage.length; i++) {
-        //     const key = localStorage.key(i);
-        //     if (key.startsWith('note-')) {
-        //         const savedText = localStorage.getItem(key);
-        //         currentNotes.push(savedText);
-        //     }
-        // }
-    
-        // for (const note of currentNotes) {
-        //     const noteDiv = document.createElement('div');
-        //     noteDiv.textContent = note;
-        //     this.row.appendChild(noteDiv);
-        // }
-    }
+}
 
 function updateNotes() {
+    updateContents();
     setInterval(() => {
-        const container = document.getElementById('notes-container');
-        container.innerHTML = '';
-        const timestamp = document.getElementById('time-stamp-read');
-        timestamp.innerHTML = 'Updated at: ' + new Date().toLocaleTimeString();
-        for (let i = 0; i < localStorage.length; i++) {
-            const key = localStorage.key(i);
-            const addReadNote = new readText(localStorage.getItem(key));
-        }
+        updateContents();
     }, 2000);
+}
+
+function updateContents(){
+    const container = document.getElementById('notes-container');
+    container.innerHTML = '';
+    const timestamp = document.getElementById('time-stamp-read');
+    timestamp.innerHTML = 'Updated at: ' + new Date().toLocaleTimeString();
+    for (let i = 0; i < localStorage.length; i++) {
+        const key = localStorage.key(i);
+        const addReadNote = new readText(localStorage.getItem(key));
+    }
 }
