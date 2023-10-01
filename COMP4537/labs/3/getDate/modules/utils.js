e7c2793b74cb803a41d0676d59fb796222e45cd6
Alissa <alissa.graham88@gmail.com>
2023-10-01 14:53:37 -0700

    attempt to pare down files to find issues

---
+++ COMP4537/labs/3/getDate/modules/utils.js
@@ -0,0 +1,5 @@
+exports.getDate = function(name) {
+    const dateTimeObject = new Date();
+    greeting = `Hello ${name}. Current server date and time is ${dateTimeObject.toDateString()} ${dateTimeObject.toTimeString()}.`
+    return greeting;
+};
