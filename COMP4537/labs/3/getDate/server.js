80d369d6cb11ab5664ec54b1d7eadda03f3fc387
Alissa <alissa.graham88@gmail.com>
2023-10-05 21:43:42 -0700

    fixed app crash error

--- COMP4537/labs/3/getDate/server.js
+++ COMP4537/labs/3/getDate/server.js
@@ -18,29 +18,29 @@ http.createServer(function (req, res) {
         let message = dt.getDate(name);
         const messageInBlue = `<p style="color: blue;">${message}</p>`;
         res.end(messageInBlue);
-    // // Part C.1 -- write to a file.txt in the readFile directory
-    // } else if (q.pathname == '/COMP4537/labs/3/writeFile/' && q.query.text) {
-    //     let text = q.query.text;
-    //     fs.appendFile(filePath, text + '\n', (appendErr) => {
-    //         if (appendErr) {
-    //             res.writeHead(500, {'Content-Type': 'text/plain' });
-    //             res.end('Unable to append to file');
-    //         } else {
-    //             res.writeHead(200, { 'Content-Type': 'text/plain' });
-    //             res.end('Text appended to file');
-    //         }
-    //     });
-    // // Part C.2 -- read from the file in the readFile directory
-    // } else if (q.pathname === '/COMP4537/labs/3/readFile/file.txt') {
-    //     fs.readFile(filePath, 'utf8', (readErr, data) => {
-    //         if (readErr) {
-    //             res.writeHead(500, { 'Content-Type': 'text/plain' });
-    //             res.end('Error reading file');
-    //         } else {
-    //             res.writeHead(200, {'Content-Type': 'text/plain'});
-    //             res.end(data);
-    //         }
-    //     })
+    // Part C.1 -- write to a file.txt in the readFile directory
+    } else if (q.pathname == '/COMP4537/labs/3/writeFile/' && q.query.text) {
+        let text = q.query.text;
+        fs.appendFile(filePath, text + '\n', (appendErr) => {
+            if (appendErr) {
+                res.writeHead(500, {'Content-Type': 'text/plain' });
+                res.end('Unable to append to file');
+            } else {
+                res.writeHead(200, { 'Content-Type': 'text/plain' });
+                res.end('Text appended to file');
+            }
+        });
+    // Part C.2 -- read from the file in the readFile directory
+    } else if (q.pathname === '/COMP4537/labs/3/readFile/file.txt') {
+        fs.readFile(filePath, 'utf8', (readErr, data) => {
+            if (readErr) {
+                res.writeHead(500, { 'Content-Type': 'text/plain' });
+                res.end('Error reading file');
+            } else {
+                res.writeHead(200, {'Content-Type': 'text/plain'});
+                res.end(data);
+            }
+        })
     } else {
         res.writeHead(404, {'Content-type': 'text/plain'});
         res.end('Not Found');
