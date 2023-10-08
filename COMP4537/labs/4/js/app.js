77d03397a6fe5898dd08e5baca91f0b385ac4c6c
Alissa <alissa.graham88@gmail.com>
2023-10-07 21:27:25 -0700

    fixed some ptential typos and scoping issues

--- COMP4537/labs/4/js/app.js
+++ COMP4537/labs/4/js/app.js
@@ -1,38 +1,48 @@
+const endpoint = 'http://www.wilwscott.com/COMP4537/labs/4/api/definitions/';
+
 function addDefinition() {
     const word = document.getElementById('word').value;
     const definition = document.getElementById('definition').value;
-    const endpoint = 'http://www.wilwscott.com/COMP4537/labs/4/api/definitions/';
+    if (!word || !definition || word.trim() === '' || definition.trim() ==='') {
+        document.getElementById('output').innerHTML = 'Please input both a word and definition to enter.';
+    } else {
+        let wordObj = {
+            word: word,
+            definition: definition
+        };
+        
+        let post = JSON.stringify(wordObj);
+        let xhttp = new XMLHttpRequest();
 
-    let wordObj = {
-        word: word,
-        definition: definition
-    };
-    
-    let post = JSON.stringify(wordObj);
-    let xhttp = new XMLHttpRequest();
+        xhttp.open('POST', endpoint, true)
+        xhttp.setRequestHeader('Content-type', 'application/json; charset=UTF-8')
+        xhttp.send(post);
 
-    xhttp.open('POST', endpoint, true)
-    xhttp.setRequestHeader('Content-type', 'application/json; charset=UTF-8')
-    xhttp.send(post);
-
-    xhttp.onload = function () {
-        if(xhttp.status === 200) {
-            console.log("Definition successfully saved!");
-        }
+        xhttp.onload = function () {
+            if(xhttp.status === 200) {
+                document.getElementById('output').innerHTML ='Definition successfully saved!';
+            }   
+        };
     };
 }
 
 function getDefinitions() {
     const wordSearch = document.getElementById('wordSearch').value;
-    const param = '?word=' + wordSearch
-    const url = endpoint + param;
-    
-    xhttp.open('GET', url, true);
-    xhttp.set();
-    xhttp.onload = function () {
-        if(xhttp.status === 200) {
-            const response = JSON.parse(this.responseText)
-            document.getElementById('wordsContainer').innerHTML = response.definition;
-        }
+    if (!wordSearch || wordSearch.trim() === '') {
+        document.getElementById('wordsContainer').innerHTML = 'Please input a word to search';
+    } else {
+        const param = '?word=' + wordSearch
+        const url = endpoint + param;
+
+        let xhttp = new XMLHttpRequest();
+        
+        xhttp.open('GET', url, true);
+        xhttp.send();
+        xhttp.onload = function () {
+            if(xhttp.status === 200) {
+                const response = JSON.parse(this.responseText)
+                document.getElementById('wordsContainer').innerHTML = response.definition;
+            }
+        };
     };
 }
