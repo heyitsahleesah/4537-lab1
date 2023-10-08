8b29fa8b00e41d4e22cfa7827edce4885d2240cf
Alissa <alissa.graham88@gmail.com>
2023-10-08 12:32:25 -0700

    add validation

--- COMP4537/labs/4/js/app.js
+++ COMP4537/labs/4/js/app.js
@@ -1,4 +1,4 @@
-// Some code checked with chatgpt
+// Some code checked and adapted from chatgpt
 
 // create endpoint for both functions
 const endpoint = 'https://www.wilwscott.com/COMP4537/labs/4/api/definitions/';
@@ -7,41 +7,44 @@ function addDefinition() {
     // get the word and definition from the html text box and area
     const word = document.getElementById('word').value;
     const definition = document.getElementById('definition').value;
-    console.log('word: ', word);
-    console.log('definition: ', definition);
+    
+    // Regular expression to disallow numbers and special characters [chatgpt]
+    const invalidChars = /[0-9!@#$%^&*()_+{}\[\]:;<>,.?~\\\/'"\-=]/;
+
 
     // create a new xmlhttprequest
     let xhttp = new XMLHttpRequest();
 
-    // if (!word || !definition || word.trim() === '' || definition.trim() ==='') {
-    //     document.getElementById('output').innerHTML = 'Please input both a word and definition to enter.';
-    // } else {
-    
-    // create params and add to endpoint url for query
-    const param = '?word=' + word + "&definition=" + definition;
-    const url = endpoint + param;
+    if (!word || !definition || word.trim() === '' || definition.trim() ==='') {
+        document.getElementById('output').innerHTML = 'Please input both a word and definition.';
+    } else if (invalidChars.test(word) || invalidChars.test(definition)) {
+        document.getElementById('output').innerHTML = 'Input contains invalid characters. Please remove them.'; 
+    } else {
+        // create params and add to endpoint url for query
+        const param = '?word=' + word + "&definition=" + definition;
+        const url = endpoint + param;
 
-    // send POST request
-    xhttp.open('POST', endpoint, true)
-    xhttp.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
-    xhttp.send(param);
+        // send POST request
+        xhttp.open('POST', endpoint, true)
+        xhttp.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+        xhttp.send(param);
 
-    let response; 
+        let response; 
 
-    // check status and display returned message
-    xhttp.onreadystatechange = function () {
-        if (xhttp.readyState === 4 && xhttp.status === 200) {
-            console.log(xhttp.responseText);
-            response = JSON.parse(xhttp.responseText);
-            document.getElementById('output').innerHTML = response.message;
-        } else if (xhttp.status === 400) {
-            document.getElementById('output').innerHTML = xhttp.responseText.message;
-        }  else {
-            document.getElementById('output').innerHTML = 'Unexpected status code' + xhttp.status;
-        }
-    };
+        // check status and display returned message
+        xhttp.onreadystatechange = function () {
+            if (xhttp.readyState === 4 && xhttp.status === 200) {
+                console.log(xhttp.responseText);
+                response = JSON.parse(xhttp.responseText);
+                document.getElementById('output').innerHTML = response.message;
+            } else if (xhttp.status === 400) {
+                document.getElementById('output').innerHTML = xhttp.responseText.message;
+            }  else {
+                document.getElementById('output').innerHTML = 'Unexpected status code' + xhttp.status;
+            }
+        };
+    }
 }
-// }
 
 function getDefinitions() {
     // get word from text box
