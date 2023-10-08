89f64cfeeb53ad4cae0ea444182424b0232ac862
Alissa <alissa.graham88@gmail.com>
2023-10-08 01:00:05 -0700

    testing

--- COMP4537/labs/4/js/app.js
+++ COMP4537/labs/4/js/app.js
@@ -1,51 +1,67 @@
+// Some code checked with chatgpt
+
+// create endpoint for both functions
 const endpoint = 'http://www.wilwscott.com/COMP4537/labs/4/api/definitions/';
 
 function addDefinition() {
+    // get the word and definition from the html text box and area
     const word = document.getElementById('word').value;
     const definition = document.getElementById('definition').value;
-    // const definitionWithoutSpaces = definition.replace(' ', '+');
+
+    // create a new xmlhttprequest
     let xhttp = new XMLHttpRequest();
 
-    if (!word || !definition || word.trim() === '' || definition.trim() ==='') {
-        document.getElementById('output').innerHTML = 'Please input both a word and definition to enter.';
-    } else {
-        const param = '?word=' + word + "&definition=" + definition;
-        const url = endpoint + param;
+    // if (!word || !definition || word.trim() === '' || definition.trim() ==='') {
+    //     document.getElementById('output').innerHTML = 'Please input both a word and definition to enter.';
+    // } else {
+    
+    // create params and add to endpoint url for query
+    const param = '?word=' + word + "&definition=" + definition;
+    const url = endpoint + param;
 
+    // send POST request
+    xhttp.open('POST', url, true)
+    xhttp.setRequestHeader('Content-type', 'application/json; charset=UTF-8')
+    xhttp.send();
 
-        xhttp.open('POST', url, true)
-        xhttp.setRequestHeader('Content-type', 'application/json; charset=UTF-8')
-        xhttp.send();
-
-        xhttp.onreadystatechange = function () {
-            if(xhttp.status === 200) {
-                const response = JSON.parse(this.responseText);
-                document.getElementById('output').innerHTML = response;
-            }   
-        };
+    // check status and display returned message
+    xhttp.onreadystatechange = function () {
+        if(xhttp.status === 200) {
+            const response = JSON.parse(this.responseText);
+            document.getElementById('output').innerHTML = response.message;
+        }   
     };
 }
+// }
 
 function getDefinitions() {
+    // get word from text box
     const wordSearch = document.getElementById('wordSearch').value;
-    if (!wordSearch || wordSearch.trim() === '') {
-        document.getElementById('wordsContainer').innerHTML = 'Please input a word to search';
-    } else {
-        const param = '?word=' + wordSearch
-        const url = endpoint + param;
+    // if (!wordSearch || wordSearch.trim() === '') {
+    //     document.getElementById('wordsContainer').innerHTML = 'Please input a word to search';
+    // } else {
 
-        let xhttp = new XMLHttpRequest();
-        
-        xhttp.open('GET', url, true);
-        xhttp.send();
-        xhttp.onreadystatechange = function () {
-        const response = JSON.parse(this.responseText)
-            if(response.status === 200) {
-                // const actualDefinition = response.definition.replace('+', ' ');
-                document.getElementById('wordsContainer').innerHTML = response.definition;
-            } else {
-                document.getElementById('wordsContainer').innerHTML = response.message;
-            }
+    // create param to add to endpoint or query
+    const param = '?word=' + wordSearch
+    const url = endpoint + param;
+
+    // create a new xmlhttprequest
+    let xhttp = new XMLHttpRequest();
+    
+    // send GET request
+    xhttp.open('GET', url, true);
+    xhttp.send();
+
+    // check for response
+    xhttp.onreadystatechange = function () {
+    const response = JSON.parse(this.responseText)
+        if(xhttp.status === 200) {
+            // print definition if successful
+            document.getElementById('wordsContainer').innerHTML = response.definition;
+        } else {
+            // print error message
+            document.getElementById('wordsContainer').innerHTML = response.message;
         }
-    };
-} 
+    }
+}
+// } 
