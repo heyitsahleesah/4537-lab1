c3a5b8f78a635cf30f8e03032089a668638ecb06
Alissa <alissa.graham88@gmail.com>
2023-10-12 22:59:34 -0700

    updated app.js with error catching for textarea and some placeholder for GET and POST

--- COMP4537/labs/5/js/app.js
+++ COMP4537/labs/5/js/app.js
@@ -43,17 +43,21 @@ function insertRows() {
 
 function queryDB() {
     // get word from text box
-    const wordSearch = document.getElementById('wordSearch').value;
+    const sqlQuery = document.getElementById('query').value;
 
     // Regular expression to disallow numbers and special characters [chatgpt]
-    const invalidChars = /[0-9!@#$%^&*()_+{}\[\]:;<>,.?~\\\/'"\-=]/;
+    const invalidChars = /[!@#$%^&()_+{}\[\]:;<>,.?~\\\/'"\-=]/;
+    
 
     // check for empty input
-    if (!wordSearch || wordSearch.trim() === '') {
-        document.getElementById('wordContainer').innerHTML = 'Please input a word.';
+    if (!sqlQuery || sqlQuery.trim() === '') {
+        document.getElementById('errorMessage').innerHTML = 'Please input a SELECT or INSERT query.';
         // check for invalid characters
     } else if (invalidChars.test(wordSearch)) {
-        document.getElementById('wordContainer').innerHTML = 'Input contains invalid characters. Please remove them.'; 
+        document.getElementById('errorMessage').innerHTML = 'Input contains invalid characters. Please remove them.'; 
+        //  check if select or insert
+    } else if (!sqlQuery.toLowerCase().startsWith("select") || !sqlQuery.toLowerCase().startsWith("insert")) {
+        document.getElementById('errorMessage').innerHTML = 'Must only be SELECT or INSERT methods. Please try again'; 
         //  proceed if input is acceptable
     } else {
         // create param to add to endpoint or query
@@ -62,21 +66,44 @@ function queryDB() {
 
         // create a new xmlhttprequest
         let xhttp = new XMLHttpRequest();
-        
-        // send GET request
-        xhttp.open('GET', url, true);
-        xhttp.send();
+        if (sqlQuery.toLowerCase().startsWith("select")){
+            // send GET request
+            xhttp.open('GET', url, true);
+            xhttp.send();
 
-        // check for response
-        xhttp.onreadystatechange = function () {
-            if (xhttp.readyState === 4 && xhttp.status === 200){
-                console.log('Response from server:', xhttp.responseText);
-                const response = JSON.parse(xhttp.responseText)
-                // print definition if successful
-                document.getElementById('wordContainer').innerHTML = "Request: " + response.count + "<br>" + response.definition;
-            } else {
-                document.getElementById('wordContainer').innerHTML = xhttp.responseText.message;
-            }
+            // check for response
+            xhttp.onreadystatechange = function () {
+                if (xhttp.readyState === 4 && xhttp.status === 200){
+                    console.log('Response from server:', xhttp.responseText);
+                    const response = JSON.parse(xhttp.responseText)
+                    // print definition if successful
+                    document.getElementById('wordContainer').innerHTML = "Request: " + response.count + "<br>" + response.definition;
+                } else {
+                    document.getElementById('wordContainer').innerHTML = xhttp.responseText.message;
+                }
+            } 
+        } else {
+            // send POST request
+            xhttp.open('POST', endpoint, true)
+            xhttp.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+            xhttp.send(param);
+
+            let response; 
+
+            // check status and display returned message
+            xhttp.onreadystatechange = function () {
+                if (xhttp.readyState === 4 && xhttp.status === 200) {
+                    console.log(xhttp.responseText);
+                    response = JSON.parse(xhttp.responseText);
+                    // TO DO: update with how the server will respond
+                    document.getElementById('response').innerHTML = "Request: " + response.count + "<br>" + response.message;
+                } else if (xhttp.status === 400) {
+                    document.getElementById('output').innerHTML = xhttp.responseText.message;
+                }  else {
+                    document.getElementById('output').innerHTML = 'Unexpected status code' + xhttp.status;
+                }
+            };
+        }
+        
         }
     } 
-}
