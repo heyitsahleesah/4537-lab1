03f9f09a36c62e9b1944b647098acb26bb7ad8c9
Alissa <alissa.graham88@gmail.com>
2023-10-15 11:15:51 -0700

    testing fix to labs 5 js and css not found

--- COMP4537/labs/5/js/client-app.js
+++ COMP4537/labs/5/js/client-app.js
@@ -3,8 +3,8 @@
 // strings for what the user would see
 const noQuery = 'Please input a SELECT or INSERT query.';
 const invalidCharacters = 'Input contains invalid characters. Please remove them.';
-const tryAgain = 'Must only be SELECT or INSERT methods. Please try again';
-const unexpected = 'Unexpected status code';
+const tryAgain = 'Must only be SELECT or INSERT methods. Please try again.';
+const unexpected = 'Unexpected status code ';
 let insertPatientStatement = 'INSERT INTO patient(name, dateOfBirth) VALUES';
 
 // variables to be used
@@ -19,11 +19,11 @@ function insertRows() {
     let i = 0;
 
    if (i < names.length) {
-        let param = insertPatientStatement += ' (' + names[i] + ', ' + birthdays[i] + ')';
+        let param = encodeURIComponent(insertPatientStatement += ' (' + names[i] + ', ' + birthdays[i] + ')');
 
         // create a new xmlhttprequest
-        let xhttp = new XMLHttpRequest();x
-
+        let xhttp = new XMLHttpRequest();
+        console.log(param);
         // send POST request
         xhttp.open('POST', endpoint, true)
         xhttp.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
@@ -61,27 +61,27 @@ function queryDB() {
 
     // check for empty input
     if (!sqlQuery || sqlQuery.trim() === '') {
-        document.getElementById('errorMessage').innerHTML = noQuery;
+        document.getElementById('returnMessage').innerHTML = noQuery;
         // check for invalid characters
     } else if (invalidChars.test(sqlQuery)) {
-        document.getElementById('errorMessage').innerHTML = invalidCharacters; 
+        document.getElementById('returnMessage').innerHTML = invalidCharacters; 
         //  check if not select or insert
     } else if (!sqlQuery.toLowerCase().startsWith("select") || !sqlQuery.toLowerCase().startsWith("insert")) {
-        document.getElementById('errorMessage').innerHTML = tryAgain; 
+        document.getElementById('returnMessage').innerHTML = tryAgain; 
         //  proceed if input is acceptable
     } else {
         if (sqlQuery.toLowerCase().startsWith("select")) {
             // create param to add to endpoint or query
             const param = encodeURIComponent(sqlQuery);
-            const url = endpoint + param;
+            // const url = endpoint + param;
 
             // create a new xmlhttprequest
             let xhttp = new XMLHttpRequest();
 
             // send GET request
-            xhttp.open('GET', url, true);
-            xhttp.send();
-
+            xhttp.open('GET', endpoint, true);
+            xhttp.send(param);
+            console.log(xhttp.send(param));
             // check for response
             xhttp.onreadystatechange = function () {
                 if (xhttp.readyState === 4 && xhttp.status === 200){
